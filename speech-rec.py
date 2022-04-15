6a221f9e43a514fa831ef069cb579ea138989da1
Kshitij Singh <kshitijsingh841@gmail.com>
2022-04-15 19:34:53 +0530

    Update speech-rec.py

--- speech-rec.py
+++ speech-rec.py
@@ -1,6 +1,32 @@
 import speech_recognition as sr
-import webbrowser
+import json
+from datetime import datetime
 
-r1 = sr.Recognizer()
-r2 = sr.Recognizer()
-r3 = sr.Recognizer()
+run = True 
+data = {}
+r = sr.Recognizer()
+
+with open("notepad.json", "r") as f:
+    data = json.load(f)
+
+while run:
+    with sr.Microphone() as source:
+        print("Recognizing.....")
+        audio_text = r.listen(source)
+        print("Done!")
+
+        try: 
+            text = r.recognize_google(audio_text)
+            print(json.dumps(data, indent = 4))
+            now = datetime.now()
+            timestamp = datetime.timestamp(now)
+            data["notes"].append({
+                str(timestamp) : text
+                })
+            with open("notepad.json", "w") as outfile:
+                outfile.write(json.dumps(data, indent = 0))
+        except KeyboardInterrupt:
+            print("Stopped")
+        except:
+            print("Couldn't Recognize")
+            run = False
